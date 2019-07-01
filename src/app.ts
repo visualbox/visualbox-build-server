9810e778e702c057d567d004bf0ab2c91c9d7986
Pwntus <pontus.edvard@gmail.com>
2019-07-02 00:59:10 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+
+import * as buildController from './controllers/build';
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', buildController.build);
+
+export default app;
---
+++ src/controllers/build.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+/**
+ * POST /
+ * Build a project.
+ */
+export const build = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+
+  } catch (e) {
+
+  }
+};
